c09a461c55f170619dcdc35accf825c4c060359d
yoonsen <yoonsen@gmail.com>
2022-09-16 00:13:09 +0200

    added

---
+++ dtm.py
@@ -0,0 +1,25 @@
+import dhlab as dh
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="DTM", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.session_state.update(st.session_state)
+
+
+
+st.header("Tell ord i korpus")
+st.markdown("Velg et korpus fra [corpus-appen](https://beta.nb.no/dhlab/corpus/)")
+
+
+
+uploaded_file = st.file_uploader("last opp korpus", help="slipp en fil, eller velg fra en mappe")
+if uploaded_file is not None:
+    dataframe = pd.read_excel(uploaded_file)
+    st.subheader('Korpus')
+    st.write(dataframe.head(3))
+    st.subheader('Dokument/term-matrise')
+    corpus = dh.Corpus(doctype='digibok',limit=0)
+    corpus.extend_from_identifiers(list(dataframe.urn))
+    dtm = dh.Counts(corpus)
+    st.write(dtm.counts)
+    st.session_state['dtm'] = dtm.counts
---
+++ pages/01_inspect.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+
+#st.session_state.update(st.session_state)
+
+df = st.session_state['dtm']
+
+#st.table(df.head())
+st.header('Inspiser termene i korpuset')
+
+with st.form("my_form"):
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        words = st.text_input("Angi ord som skal sammenlignes", "", help="Skriv inn ordene skilt med komma. For å ta med komma, legg inn et til slutt")
+        words = [w.strip() for w in words.split(',')]
+        if "" in words:
+            words.append(',')
+        words = [w for w in words if w != ""]
+        
+        
+    with col2:
+        sort_col = st.number_input("Sorter etter kolonne", min_value = 0, max_value = len(df.columns) - 1, value = 0)
+    
+    #st.write(words)
+
+    submitted = st.form_submit_button("klikk når alt er klart")
+    #st.write(words)
+    if words == [","]:
+        tbl = df.sample(10)
+    else:
+        tbl = df.loc[[w for w in words if w in df.index]]
+    if submitted:
+        st.table(
+            tbl.sort_values(by = tbl.columns[0], ascending = False).head(50).style.background_gradient()
+        )
